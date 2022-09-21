b22f92beb0e9e37d98fadc9c05ba1bacfc671f2d
Timba <thiagodesouza07t@gmail.com>
2022-09-21 12:16:29 -0300

    juntar testes

--- get_test.go
+++ get_test.go
@@ -4,12 +4,31 @@ import (
 	"testing"
 )
 
+type viacepTests struct {
+	name     string
+	url      string
+	expected string
+}
+
 func TestGetCorrect(t *testing.T) {
 
 	tt := viacepTests{
 		name:     "Get 13099160 info",
 		url:      "https://viacep.com.br/ws/13099160/json/",
 		expected: "Campinas",
+	}
+
+	if got := GetRequest(tt.url); got.Localidade != tt.expected {
+		t.Errorf("GetRequest(%s) = %s; want %s", tt.url, got.Localidade, tt.expected)
+	}
+}
+
+func TestGetWrong(t *testing.T) {
+
+	tt := viacepTests{
+		name:     "Get 89099160 Wrong/Nil info",
+		url:      "https://viacep.com.br/ws/89099160/json/",
+		expected: "",
 	}
 
 	if got := GetRequest(tt.url); got.Localidade != tt.expected {
